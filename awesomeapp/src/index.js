00f155e47472a9ec97537e78b0d0b441fafc525f
msdesai1499 <msdesai@mitaoe.ac.in>
2021-07-12 22:26:00 +0530

    commit

--- awesomeapp/src/index.js
+++ awesomeapp/src/index.js
@@ -4,4 +4,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<h1>Hello World!!</h1>, document.getElementById('root'));
+ReactDOM.render(
+	<>
+		<h1>Netflix Pick...</h1>
+		<p>List of 5 best Series</p>
+		<ol>
+			<li>Dark</li>
+			<li>Extra Curricular</li>
+			<li>My Holo Love</li>
+			<li>Mr Robot</li>
+			<li>My First-2 Love</li>
+		</ol>
+	</>,
+	document.getElementById('root'));
+
+// var h1 = document.createElement('h1');
+// h1.innerHTML = `Hello Welcome`;
+// document.getElementById('root').appendChild(h1);
